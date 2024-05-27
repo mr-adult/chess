43792d6078e9240ad7e8e7effa6cefb7ec72d1de
Adam Fortune <adamfortune96@protonmail.com>
2024-05-27 16:39:31 -0500

    Move piece.rs to common

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -1,9 +1,6 @@
 mod bitboard;
 mod board;
 mod chess_move;
-mod piece;
 
-use chess_move::Move;
-
+pub use chess_move::Move;
 pub use board::Board;
-pub use piece::Piece;
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,9 +3,11 @@ mod file;
 mod rank;
 mod player;
 mod piece_kind;
+mod piece;
 
 pub use location::Location;
 pub use file::File;
 pub use rank::Rank;
 pub use piece_kind::PieceKind;
 pub use player::Player;
+pub use piece::Piece;
