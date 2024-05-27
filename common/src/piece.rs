02ecd53691ab165050c61a9584dc7fd108ce8f4d
Adam Fortune <adamfortune96@protonmail.com>
2024-05-27 16:38:38 -0500

    Move Piece.rs into common

---
+++ common/src/piece.rs
@@ -0,0 +1,39 @@
+use std::fmt::Debug;
+
+use crate::{PieceKind, Player};
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Piece {
+    kind: PieceKind,
+    player: Player,
+}
+
+impl Piece {
+    pub fn new(player: Player, kind: PieceKind) -> Self {
+        Self { kind, player }
+    }
+
+    pub const fn kind(&self) -> PieceKind {
+        self.kind
+    }
+
+    pub const fn player(&self) -> Player {
+        self.player
+    }
+
+    pub const fn to_fen(&self) -> char {
+        match self.player() {
+            Player::White => self.kind().as_char().to_ascii_uppercase(),
+            Player::Black => self.kind().as_char().to_ascii_lowercase(),
+        }
+    }
+}
+
+impl Debug for Piece {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut repr_string = String::with_capacity(3);
+        repr_string.push(self.player.as_char());
+        repr_string.push(self.kind.as_char());
+        f.write_str(&repr_string)
+    }
+}
