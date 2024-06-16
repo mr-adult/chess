d114f6dc21d7751a442370d6afcd4efd95298a07
Adam Fortune <adamfortune96@protonmail.com>
2024-06-16 15:13:15 -0500

    Implement Deserialize for Location and remove unused code

--- chess/src/chess_move.rs
+++ chess/src/chess_move.rs
@@ -1,45 +1,8 @@
-use std::str::FromStr;
-
 use chess_common::Location;
-use serde_derive::Serialize;
+use serde_derive::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Move {
     pub(crate) from: Location,
     pub(crate) to: Location,
-}
-
-pub(crate) enum PawnMoveKind {
-    Push,
-    DoublePush,
-    Capture,
-    EnPassant,
-    Promotion,
-    CapturePromotion,
-}
-
-pub(crate) enum KingMoveKind {
-    Normal,
-    Castles,
-}
-
-pub(crate) enum MoveKind {
-    /// A move where the piece starts and ends at the same
-    /// position, resulting in no net change to the board.
-    NonMove,
-    /// A standard move.
-    Normal,
-    /// A pawn move. This is separate from normal so that
-    /// en passant can be correctly classified.
-    Pawn(PawnMoveKind),
-    /// A king move. This is separate from normal so that 
-    /// castling can be correctly classified.
-    King(KingMoveKind),
-}
-
-pub(crate) enum IllegalMove {
-    NoPieceAtFromLocation,
-    PieceOwnedByOtherPlayer,
-    IllegalMove,
-    Check,
 }
--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -3,5 +3,5 @@ mod bitboard;
 mod board;
 mod chess_move;
 
-pub use chess_move::Move;
 pub use board::Board;
+pub use chess_move::Move;
